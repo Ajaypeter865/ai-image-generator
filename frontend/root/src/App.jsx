79481ad1935b1d385b0bc7745a59a95cca9f56b1
AjayPeter865 <ajaypeter865@gmail.com>
2026-01-29 11:43:00 +0530

    logo and icon

--- frontend/root/src/App.jsx
+++ frontend/root/src/App.jsx
@@ -1,43 +1,3 @@
-// import { useState } from 'react'
-// import './App.css'
-
-// function App() {
-//   const [prompt, setPromt] = useState("")
-//   const [image, setImage] = useState("")
-
-//   const generateImage = async () => {
-//     const res = await fetch('http://localhost:8000/', {
-//       method: 'POST',
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ prompt })
-//     })
-//     const data = await res.json()
-//     setImage(data.image)
-//   }
-
-//   return (
-//     <div className="app-container">
-//       <div className="card">
-//         <h1>Enter your prompt</h1>
-
-//         <input
-//           type="text"
-//           placeholder="Enter text"
-//           onChange={(e) => setPromt(e.target.value)}
-//         />
-
-//         <button onClick={generateImage}>Generate</button>
-
-//         {image && <img src={image} alt="Generated" />}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
 import { useState } from 'react'
 import './App.css'
 
@@ -96,3 +56,6 @@ function App() {
 }
 
 export default App
+
+
+
